254ae2bdd69d95d291f0f86bd9ee1d98402fc928
Raj Shah <rshah28@ncsu.edu>
2021-08-09 03:15:47 -0400

    Added CNAME

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -5,7 +5,9 @@ const sh = require('shelljs');
 
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
+    const cnamePath = upath.resolve(upath.dirname(__filename), '../CNAME');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
     
     sh.cp('-R', sourcePath, destPath)
+    sh.cp(cnamePath, destPath);
 };
